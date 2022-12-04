1b0b348a038ceecb367b6909e2afe072223f6669
Robson <sc.robson@gmail.com>
2022-12-05 00:32:37 +0100

    Add template for easy copy

---
+++ AdventOfCode/DayX/DayX.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode.DayX {
+    public static class DayX {
+        public static void Go() {
+            var input = File.ReadLines("DayX/Input.txt");
+
+            Console.WriteLine("Day X, Star 1: {0}", 0);
+            Console.WriteLine("Day X, Star 2: {0}", 0);
+        }
+    }
+}
