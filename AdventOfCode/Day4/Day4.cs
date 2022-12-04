4d047697d375bc1d18eb0050e4b6d9de12df628c
Robson <sc.robson@gmail.com>
2022-12-05 00:32:17 +0100

    Day 4 ✅

---
+++ AdventOfCode/Day4/Day4.cs
@@ -0,0 +1,33 @@
+﻿namespace AdventOfCode.Day4 {
+    public static class Day4 {
+        public static void Go() {
+            var input = File.ReadLines("Day4/Input.txt");
+            var assignmentPairs = input.Select(x => new AssignmentPair(x));
+
+            Console.WriteLine("Day 4, Star 1: {0}", assignmentPairs.Count(x => x.HasFullOverlap()));
+            Console.WriteLine("Day 4, Star 2: {0}", assignmentPairs.Count(x => x.HasAnyOverlap()));
+        }
+    }
+
+    public class AssignmentPair {
+        public AssignmentPair(string line) {
+            var pairs = line.Split(',');
+            var firstPair = pairs[0].Split('-').Select(x => int.Parse(x));
+            var secondPair = pairs[1].Split('-').Select(x => int.Parse(x));
+
+            SectionsFirstPair = Enumerable.Range(firstPair.First(), firstPair.Last() - firstPair.First() + 1);
+            SectionsSecondPair = Enumerable.Range(secondPair.First(), secondPair.Last() - secondPair.First() + 1);
+        }
+
+        public IEnumerable<int> SectionsFirstPair { get; }
+        public IEnumerable<int> SectionsSecondPair { get; }
+
+        public bool HasFullOverlap() {
+            return SectionsFirstPair.All(x => SectionsSecondPair.Contains(x)) || SectionsSecondPair.All(x => SectionsFirstPair.Contains(x));
+        }
+
+        public bool HasAnyOverlap() {
+            return SectionsFirstPair.Any(x => SectionsSecondPair.Contains(x)) || SectionsSecondPair.Any(x => SectionsFirstPair.Contains(x));
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,9 +1,11 @@
-using AdventOfCode.Day1;
+﻿using AdventOfCode.Day1;
 using AdventOfCode.Day2;
 using AdventOfCode.Day3;
+using AdventOfCode.Day4;
 
 Day1.Go();
 Day2.Go();
 Day3.Go();
+Day4.Go();
 
 Console.Read();
