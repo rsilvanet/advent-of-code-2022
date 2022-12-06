6c59c007357992b0c1d482cf49d1cb0b110a3bd7
Robson <sc.robson@gmail.com>
2022-12-06 07:31:39 +0100

    Day 6 ✅

---
+++ AdventOfCode/Day6/Day6.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Day6 {
+    public static class Day6 {
+        public static void Go() {
+            var input = File.ReadAllText("Day6/Input.txt");
+
+            Console.WriteLine("Day 6, Star 1: {0}", GetMarkerPosition(input, 4));
+            Console.WriteLine("Day 6, Star 2: {0}", GetMarkerPosition(input, 14));
+        }
+
+        private static int GetMarkerPosition(string input, int distinctAmount) {
+            for (int i = distinctAmount - 1; i < input.Length; i++) {
+                if (input.Skip(i - distinctAmount).Take(distinctAmount).Distinct().Count() == distinctAmount) {
+                    return i;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -3,11 +3,13 @@ using AdventOfCode.Day2;
 using AdventOfCode.Day3;
 using AdventOfCode.Day4;
 using AdventOfCode.Day5;
+using AdventOfCode.Day6;
 
 Day1.Go();
 Day2.Go();
 Day3.Go();
 Day4.Go();
 Day5.Go();
+Day6.Go();
 
 Console.Read();
